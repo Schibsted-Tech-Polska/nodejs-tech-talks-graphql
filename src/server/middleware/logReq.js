11c4687d495deb5e7ba86ddfa6bf8b8634997fee
Krzysztof Rudowski <krzysztof.rudowski@schibsted.pl>
2019-06-24 16:53:56 +0200

    update logging requests

--- src/server/middleware/logReq.js
+++ src/server/middleware/logReq.js
@@ -1,7 +1,7 @@
 const logger = require('../logger');
 
 module.exports = async (ctx, next) => {
-    logger.debug('<-', ctx.request.method, ctx.request.url);
+    logger.debug('<-', ctx.request.method, decodeURIComponent(ctx.request.url));
 
     if (ctx.request.body && ctx.request.body.query) {
         logger.debug(ctx.request.body.query.replace('\\n', '\n'));
