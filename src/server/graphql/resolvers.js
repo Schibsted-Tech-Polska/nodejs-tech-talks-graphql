2d2237f1f44afb6f0aefe81912470e9ccb424773
Krzysztof Rudowski <rudi23@o2.pl>
2019-06-25 12:52:20 +0200

    Use dataloaders (#6)
    * use dataloaders

--- src/server/graphql/resolvers.js
+++ src/server/graphql/resolvers.js
@@ -1,13 +1,20 @@
 /* eslint-disable no-return-await */
+const DataLoader = require('dataloader');
 const categoryQueries = require('../db/category');
 const productQueries = require('../db/product');
+
+const productsLoader = new DataLoader(categoryIds =>
+    productQueries
+        .fetchByCategoryIds(categoryIds)
+        .then(rows => categoryIds.map(id => rows.filter(item => item.category_id === id)))
+);
 
 module.exports = {
     Query: {
         categories: async () => await categoryQueries.fetchAll(),
     },
     Category: {
-        products: async category => await productQueries.fetchByCategoryId(category.id),
+        products: async category => await productsLoader.load(category.id),
     },
 };
 
